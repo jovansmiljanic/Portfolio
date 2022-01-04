313a539b203075a0792a7c2df20273747e101004
Jovan Smiljanic <jovan.smiljanic@consalta.si>
2022-01-04 22:17:09 +0100

    ENV fix

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,8 +1,6 @@
 import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
 
-// require("dotenv").config();
-
 export default function Form() {
   const form = useRef(null);
 
